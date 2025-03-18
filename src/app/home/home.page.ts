217ec013dfd2be62ecfe07c39166b9aa43a3fe76
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-18 11:07:33 -0300

    Initial commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RangeCustomEvent } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: false,
+})
+export class HomePage {
+
+  constructor() {}
+
+  valorRange = 0;
+
+  onIonRange(ev:RangeCustomEvent){
+    this.valorRange = parseInt(ev.detail.value.toString())
+  };
+}
