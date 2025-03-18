fcf7e2267162b01cb6a418a3d5d17fdddf61b30f
ibpds <pordeus.school@gmail.com>
2025-03-18 11:44:03 -0300

    atualizando pt2

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,8 +12,15 @@ export class HomePage {
   constructor() {}
 
   valorRange = 0;
+  inputValor = '';
+  res = '';
 
   onIonRange(ev:RangeCustomEvent){
-    this.valorRange = parseInt(ev.detail.value.toString())
+    this.valorRange = parseInt(ev.detail.value.toString());
   };
+
+  calcular(){
+    this.res = (this.valorRange*parseInt(this.inputValor)).toString();
+  }
+
 }
